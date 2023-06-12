b65a2127c9d4ea1618773eba95b8d60c8029f94d
Naftali Kizner <76939353+naftali10@users.noreply.github.com>
2023-06-12 16:53:55 +0300

    Checking input arguments validity

--- code/Main.py
+++ code/Main.py
@@ -7,22 +7,34 @@ def produce_pdf(filepath, domain):
     print("Started processing", filepath)
     tablet_chord_writer = TabletPDFWriter(filepath, domain)
     file_name = os.path.splitext(os.path.basename(filepath))[0]
-    pdf_file_path = 'output_pdfs/' + file_name + '.pdf'
+    pdf_file_path = '../output_pdfs/' + file_name + '.pdf'
     tablet_chord_writer.make_pdf(pdf_file_path)
     print("Saved PDF", pdf_file_path)
 
 
-def produce_pdfs(urls_dir, domain):
+def produce_pdfs(urls_dir, domain_name):
     for urls_file in os.listdir(urls_dir):
         filepath = os.path.join(urls_dir, urls_file)
         if not os.path.isfile(filepath):
             continue
-        produce_pdf(filepath, domain)
+        produce_pdf(filepath, domain_name)
+
+
+def check_path(urls_path):
+    if not os.path.exists(urls_path):
+        raise ValueError(f"Path specified ({urls_path}) does not exist")
+
+
+def check_domain(urls_domain):
+    if urls_domain != 'UG' and urls_domain != 'TAB4U':
+        raise ValueError(f"Domain specified ({urls_domain}) is illegal. Should be only UG or TAB4U.")
 
 
 if __name__ == '__main__':
     path = sys.argv[1]
     domain = sys.argv[2]
+    check_path(path)
+    check_domain(domain)
     if os.path.isdir(path):
         produce_pdfs(path, domain)
     elif os.path.isfile(path):
