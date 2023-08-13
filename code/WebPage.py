7cd47538329d4385b6dfcdd2526f572ac63bb832
Naftali Kizner <76939353+naftali10@users.noreply.github.com>
2023-08-13 18:39:47 +0200

    TDD of WebPage

--- code/WebPage.py
+++ code/WebPage.py
@@ -1,45 +1,19 @@
-from Song import Song
-from WebAux import WebAux
+from selenium import webdriver
+from bs4 import BeautifulSoup
 
 
 class WebPage:
 
     _url: str
-    _artist: str
-    _song_name: str
-    _song_text: str
-    _web_aux: WebAux
-    _language: str
-    _song: Song
-    _chord_transpose: int = 0
+    _content: BeautifulSoup
     
-    def __init__(self, url, browser):
-        url_breakup = url.split('|')
-        if 1 < len(url_breakup):
-            self._chord_transpose = int(url_breakup[-1])
-        self._url = url_breakup[0]
-        self._web_aux = WebAux(self._url, browser)
+    def __init__(self, url: str) -> None:
+        self._url = url
 
-    def __eq__(self, other: 'WebPage'):
+    def __eq__(self, other: 'WebPage') -> bool:
         return (
-                self._url == other._url and
-                self._artist == other._artist and
-                self._song_name == other._song_name and
-                self._language == other._language and
-                self._chord_transpose == other._chord_transpose
+                self._url == other._url
         )
 
-    def parse_song(self, max_line_len):
-        self._song = Song(self._song_text, max_line_len)
-
-    def get_title(self):
-        return self._artist + ' - ' + self._song_name
-
-    def get_song(self):
-        return self._song
-
-    def get_browser(self):
-        return self._web_aux.get_browser()
-
-    def get_soup(self):
-        return self._web_aux.get_soup()
+    def store_content(self, browser: webdriver.Edge) -> None:
+        self._content = BeautifulSoup(browser.page_source, "html.parser")
---
+++ tests/test_WebPage.py
@@ -0,0 +1,27 @@
+import unittest
+from WebPage import WebPage
+from selenium import webdriver
+from bs4 import BeautifulSoup
+
+
+class TestWebPage(unittest.TestCase):
+
+    def setUp(self) -> None:
+        self.url = 'https://tabs.ultimate-guitar.com/tab/major-lazer/lean-on-chords-1734407|+4'
+        self.w = WebPage(self.url)
+        self.ref = WebPage(self.url)
+
+    def test_eq(self) -> None:
+        self.assertEqual(self.w, self.ref)
+
+    def test_store_content(self) -> None:
+        options = webdriver.EdgeOptions()
+        browser = webdriver.Edge(options=options)
+        browser.get(self.url)
+        soup = BeautifulSoup(browser.page_source, "html.parser")
+        self.w.store_content(browser)
+        self.assertEqual(self.w._content, soup)
+
+
+if __name__ == '__main__':
+    unittest.main()
